aca93e94047ad0a17535c268586f05e3654959b7
Minn Thant <95152361+Thant998@users.noreply.github.com>
2024-04-01 15:43:39 +0630

    Update ATM.java

--- ATM-Machine-master/ATM/ATM.java
+++ ATM-Machine-master/ATM/ATM.java
@@ -1,13 +1,11 @@
 import java.io.IOException;
 
 public class ATM {
-
 	public static void main(String[] args) throws IOException {
 		OptionMenu optionMenu = new OptionMenu();
 		introduction();
 		optionMenu.mainMenu();
 	}
-
 	public static void introduction() {
 		System.out.println("Welcome to the ATM Project!");
 	}
